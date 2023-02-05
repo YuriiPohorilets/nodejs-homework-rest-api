dd620ce1c392826385c45c86350db51984d2d8d4
SayGoodnight <102477210+YuriiPohorilets@users.noreply.github.com>
2023-02-05 21:07:09 +0200

    added register, login, logout routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,7 @@
-const { getContactsController } = require('./getContactsController');
-const { getContactByIdController } = require('./getContactByIdController');
-const { deleteContactController } = require('./deleteContactController');
-const { createContactController } = require('./createContactController');
-const { updateContactController } = require('./updateContactController');
-const { updateStatusController } = require('./updateStatusContactController');
+const contacts = require('./contacts');
+const auth = require('./auth');
 
 module.exports = {
-  getContactsController,
-  getContactByIdController,
-  deleteContactController,
-  createContactController,
-  updateContactController,
-  updateStatusController,
+  contacts,
+  auth,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,19 @@
 const express = require('express');
-const {
-  getContactsController,
-  getContactByIdController,
-  deleteContactController,
-  createContactController,
-  updateContactController,
-  updateStatusController,
-} = require('../../controllers/index');
+const { contacts: ctrl } = require('../../controllers/index');
+const { userAuth } = require('../../middlewares');
 
 const router = express.Router();
 
-router.get('/', getContactsController);
+router.get('/', userAuth, ctrl.getContacts);
 
-router.get('/:contactId', getContactByIdController);
+router.get('/:contactId', userAuth, ctrl.getContactById);
 
-router.post('/', createContactController);
+router.post('/', userAuth, ctrl.createContact);
 
-router.delete('/:contactId', deleteContactController);
+router.delete('/:contactId', userAuth, ctrl.deleteContact);
 
-router.put('/:contactId', updateContactController);
+router.put('/:contactId', userAuth, ctrl.updateContact);
 
-router.patch('/:contactId/favorite', updateStatusController);
+router.patch('/:contactId/favorite', userAuth, ctrl.updateStatus);
 
 module.exports = router;
